0d704a287c60ca69af3fae2610ec4e64c4971f94
Gurminder singh <123150161+gurmindersingh5@users.noreply.github.com>
2024-03-28 02:33:39 -0400

    Update install_k8s.sh

--- install_k8s.sh
+++ install_k8s.sh
@@ -1,26 +1,31 @@
 #!/bin/bash
 
 ####################################
-# script to install and configurin k8s on master node using new repos from pkgs.k8s.io as of MARCH 2024
+# script to install and configurin k8s on MASTER NODE using new repos from pkgs.k8s.io as of MARCH 2024
 ####################################
 
-sudo apt update && sudo apt upgrade -y
+sudo su 
+apt update && sudo apt upgrade -y
 
-sudo apt install docker.io
-sudo systemctl start docker
-sudo systemctl enable docker
+apt install docker.io
+systemctl start docker
+systemctl enable docker
+usermod -aG docker $USER
 
+exit
 echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
-sudo apt update
-sudo apt install kubeadm kubelet kubectl
-sudo apt-mark hold kubelet kubeadm kubectl
+sudo su 
+apt update
+apt install kubeadm kubelet kubectl
+apt-mark hold kubelet kubeadm kubectl
 
-sudo swapoff -a
-sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
-sudo kubeadm init
+swapoff -a
+sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+kubeadm init
 
+exit
 mkdir -p $HOME/.kube
 sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
