565c8f90d7398f2daaa98e599739922237338c23
Gurminder singh <123150161+gurmindersingh5@users.noreply.github.com>
2024-03-28 20:28:22 -0400

    Update install_k8s.sh

--- install_k8s.sh
+++ install_k8s.sh
@@ -13,8 +13,8 @@ sudo systemctl enable docker
 sudo usermod -aG docker $USER
 
 
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
-curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.29.3/deb/ /" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.29.3/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
 
 
 sudo apt update
